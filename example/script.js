a757b9b3471c3f4155dcee9c000394fc47f7377d
simov <simeonvelichkov@gmail.com>
2016-08-21 13:30:58 +0300

    Fix indentation

--- example/script.js
+++ example/script.js
@@ -6,16 +6,16 @@ require('nw.gui').Window.get().showDevTools().resizeTo(800, 1000);
 var aw = require('../lib');
 
 var spawn = require('child_process').spawn,
-    child = spawn('node', ['spawn.js']);
+  child = spawn('node', ['spawn.js']);
 
 
 child.stdout.on('data', function (e) {
-    var str = e.toString().trim();
-    console.log(str);
-    console.log.apply(console, aw.parse(str));
+  var str = e.toString().trim();
+  console.log(str);
+  console.log.apply(console, aw.parse(str));
 });
 
 child.stderr.on('data', function (e) {
-    var str = e.toString().trim();
-    console.log(str);
+  var str = e.toString().trim();
+  console.log(str);
 });
